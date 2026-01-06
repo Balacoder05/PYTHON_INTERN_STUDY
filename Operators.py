17ba6a939e5ee94441896e2c5683b20b8ec26caa
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 17:37:26 +0530

    Operators

--- Operators.py
+++ Operators.py
@@ -25,6 +25,15 @@ print(x // y)  #Division without remainder (only integer part),15 ÷ 4 → 3 (ig
 
 # Assignment operators are used to assign values to variables:
 
+
+# Walrus Operator
+
+# The Walrus Operator := is used to assign a value to a variable as part of an expression.
+
+if (n := len("Python")) > 5:
+    print("Length is", n)
+
+
 x = 10;
 
 x += 5; 
@@ -50,6 +59,18 @@ print("x **= 3 ->", x);
 
 
 # Comparison Operators
+
+# Comparison operators are used to compare two values
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
 
 
 
