db3ec354e5642cc6437e5dbabded18ffabb2eeb6
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 16:19:24 +0530

    string modification

--- string.py
+++ string.py
@@ -78,3 +78,52 @@ print(text[0:3])
 
 text1="programming"
 print(text1[:7])
+
+
+# Slice To the End
+
+text2="balamurugan"
+print(text2[2:])
+
+
+# Negative Indexing
+
+text3="nothing"
+print(text3[-5:-1])
+
+
+
+
+# Python - Modify Strings
+
+
+# built-in methods
+
+# Upper Case
+
+text4="balamurugan"
+print(text4.upper())
+
+#lower
+
+a = "Hello, World!"
+print(a.lower())
+
+
+# Remove Whitespace
+
+c=" mannargudi! "
+print(c.strip())
+
+
+#Replace String
+
+d="Bala"
+print(d.replace("B","b"))
+
+# Split String
+
+e="balamurugan ,elangovan"
+print(e.split())
+
+
