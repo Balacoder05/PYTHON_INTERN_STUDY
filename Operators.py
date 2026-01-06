a632814bc219b2c63755f03203c1e695f9154409
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 17:31:57 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,57 @@
+# Python Operators
+
+# Operators are used to perform operations on variables and values.
+
+print(10+20)
+
+# Arithmetic Operators
+
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)  #/ - Division (returns a float)
+print(x % y)
+print(x ** y)  #15⁴ = 15 × 15 × 15 × 15 = 50625
+print(x // y)  #Division without remainder (only integer part),15 ÷ 4 → 3 (ignore 0.75),Output: 3
+# // - Floor division (returns an integer)
+
+
+# Assignment Operators
+
+# Assignment operators are used to assign values to variables:
+
+x = 10;
+
+x += 5; 
+print("x += 5 ->", x); 
+
+x -= 3; 
+print("x -= 3 ->", x);
+
+x *= 2; 
+print("x *= 2 ->", x);
+
+x /= 4; 
+print("x /= 4 ->", x);
+
+x %= 3; 
+print("x %= 3 ->", x);
+
+x //= 2; 
+print("x //= 2 ->", x);
+
+x **= 3; 
+print("x **= 3 ->", x);
+
+
+# Comparison Operators
+
+
+
+
+
