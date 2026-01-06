f2c51e2ac4d598626c9607995c8e2ecba6e46de8
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 17:48:33 +0530

    Operators

--- Operators.py
+++ Operators.py
@@ -73,6 +73,26 @@ print(x >= y)
 print(x <= y)
 
 
+# Logical Operators
+
+# and 
+
+x=5
+
+print(x > 3 and  x < 10)
+
+# or
+
+x = 5
+
+print(x > 3 or x < 4)
+
+# not
+
+x = 5
+
+print(not(x > 3 and x < 10))
 
 
+# Identity Operators
 
