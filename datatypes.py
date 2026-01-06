c6a49e7f4216877f6bed58fac49c00a38b50942c
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 15:26:39 +0530

    random module

--- datatypes.py
+++ datatypes.py
@@ -61,3 +61,40 @@ g=3+4j #complex
 print(type(e))
 print(type(f))
 print(type(g))
+
+
+
+
+# Type Conversion
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Random Number
+
+import random
+
+print(random.randrange(1,100))
+
+
+a=[1,2,3,4,5,6,7,8,9,10]
+print(random.choice(a))
