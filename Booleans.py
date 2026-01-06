10e280a4fce65f989db135d4eda7d8ed3bfaf211
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 17:15:18 +0530

    Booleans

--- Booleans.py
+++ Booleans.py
@@ -3,3 +3,48 @@
 print(10>6)
 
 print(20>30)
+
+
+
+# Evaluate Values and Variables
+
+print(bool("Hello"))
+print(bool(15))
+
+# print(bool(0)) false
+
+# Most Values are True
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+
+# Some Values are False
+
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+# isinstance()
+
+x=100
+print(isinstance(x,int))
+
+
+
