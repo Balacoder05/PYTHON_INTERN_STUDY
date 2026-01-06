6de76d28bbd11a140b2206ca905810cd6a822022
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 16:07:04 +0530

    string slicing

--- string.py
+++ string.py
@@ -23,3 +23,58 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(b)
+
+# Strings are Arrays
+
+
+a = "Hello, World!"
+print(a[1])
+
+
+# Looping Through a String
+
+for i in "balamurugan":
+    print(i,end=" ")
+
+
+# String Length
+
+c="balamurugan"
+print(len(c))
+
+
+# Check String(in)
+
+d="python programming is very easy"
+print("python" in d)
+
+# Use it in an if statement:
+
+e="python programming in english"
+if "english" in e:
+    print("yes,'english' is present")
+
+
+# Check if NOT (not in)
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+# Use it in an if statement:
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+# Python - Slicing Strings
+
+text="python"
+print(text[0:3])
+
+# Slice From the Start
+
+text1="programming"
+print(text1[:7])
