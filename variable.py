2d3cf6bb2ce1a8812ddabdf00fe18a6fb6ba7ae2
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 14:32:09 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,104 @@
+#variable
+
+#Creating Variables
+
+
+x=5
+y="Bala"
+
+print(x)
+print(y)
+
+
+# Casting
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+# Get the Type
+
+x = 5
+y = "bala"
+print(type(x))
+print(type(y))
+
+
+# Case-Sensitive
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+print(a)
+print(A)
+
+
+# Variable Names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+
+
+# Multi Words Variable Names
+
+# Camel Case
+
+myVariableName = "John"
+
+
+# Pascal Case
+
+MyVariableName = "John"
+
+
+# Snake Case
+
+
+my_variable_name = "John"
+
+
+# Many Values to Multiple Variables
+
+x,y,z="Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+
+# One Value to Multiple Variables
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+# Unpack a Collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+# Output Variables
+
+print("Bala")
