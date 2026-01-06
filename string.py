1be4dfdeb67460f5019236f623a0a275d81e1bf8
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 15:51:30 +0530

    string

---
+++ string.py
@@ -0,0 +1,25 @@
+# Strings
+
+print('hello')
+print("Hello")
+
+#Quotes Inside Quotes
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#Assign String to a Variable
+
+a="Hello"
+print(a)
+
+
+# Multiline Strings
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
