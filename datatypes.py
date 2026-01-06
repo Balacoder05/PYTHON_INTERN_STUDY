24a773c48fb49e9a058b6eb5fe29c863e2a84c38
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-06 15:11:33 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,46 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+a=10
+print(type(a))
+
+
+b=20.5
+
+print(type(b))
+
+c=5+20j
+
+print(c)
+
+print(type(c))
+
+x=memoryview(bytes(5))
+print(x)
+print(type(x))
+
+
+
+
+# Setting the Data Type
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+# Setting the Specific Data Type
+
+
+d=str("printing string")
+print(type(d))
