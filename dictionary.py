46922798783c0ca446c3b88ead796aad023978fd
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-07 15:29:05 +0530

    set  methods

---
+++ dictionary.py
@@ -0,0 +1,140 @@
+#Dictionary
+
+dict1={"name":"Bala","age":25}
+
+print(dict1)
+
+#Accessing Items
+
+print(dict1["name"])
+
+print(dict1.get("age"))
+
+
+#Get Keys
+
+print(dict1.keys())
+
+#Get Values
+
+print(dict1.values())
+
+#get items
+
+print(dict1.items())
+
+
+# Check if Key Exists
+
+
+dict2={"name":"Bala","age":25}
+if "name" in dict2:
+    print("yes,'name' is present in dict2")
+
+# Change Values
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+print(thisdict)
+
+
+# Update Dictionary
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+print(thisdict)
+
+
+#add items
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+
+#pop()  method removes the item with the specified key name
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+
+#popitem()     method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+
+# del (removes the item with the specified key name)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+
+
+# clear() (method empties the dictionary)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+
+# Copy a Dictionary
+
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+
+
+# Nested Dictionaries
+
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+
