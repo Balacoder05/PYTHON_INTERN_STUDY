8482014892ebeba0f1579e882a5e6b7c7e325475
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-01-07 09:59:00 +0530

    List and its methods

--- list.py
+++ list.py
@@ -50,3 +50,95 @@ print(list5)
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
+
+
+# Add List Items
+
+# append
+
+# To add an item to the end of the list, use the append()
+
+list6=["orange","apple","mango"]
+
+list6.append("bala")
+
+print(list6)
+
+
+
+# Insert Items
+
+# To insert a list item at a specified index, use the insert() method.
+
+list7=["bala","vicky","arun"]
+list7.insert(1,"guru")
+print(list7)
+
+
+# Extend List
+
+# To append elements from another list to the current list, use the extend() method.
+
+list1=["apple","orange","mango"]
+list2=["car","van","bike"]
+
+list1.extend(list2)
+print(list1)
+
+
+# Add Any Iterable
+
+# The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+
+list3=[1,2,3,4]
+tuple1=("bala","sri")
+
+list3.extend(tuple1)
+
+print(list3)
+
+
+# Remove List Items
+
+# The remove() method removes the specified item.
+
+
+list4=["banana","car","van"]
+
+list4.remove("car")
+
+print(list4)
+
+# Remove Specified Index (pop)
+
+list5=[1,2,3,4,5]
+list5.pop(1)
+print(list5)
+
+
+#del
+
+# The del keyword also removes the specified index
+
+
+# list6=[1,2,3,4,5]
+# del list6
+# print(list6)
+
+
+# Clear the List
+
+# he clear() method empties the list.
+
+list7=["bala","sri","vignesh"]
+list7.clear()
+print(list7)
+
+
+# Loop Lists
+
+a=[1,2,3,4,5]
+for i in a:
+    print(i)
+
+
