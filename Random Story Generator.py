2d2965e23f16a0709bb80ae6d45be40e00a024f5
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-03-27 23:47:22 +0530

    added

---
+++ Random Story Generator.py
@@ -0,0 +1,7 @@
+import random
+
+subjects = ["Dog", "Cat", "Robot", "Teacher"]
+actions = ["eats", "writes", "jumps over", "runs to"]
+objects = ["pizza", "homework", "car", "tree"]
+
+print(random.choice(subjects), random.choice(actions), random.choice(objects))
