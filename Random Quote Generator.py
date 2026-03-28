57eb09f62ec96e2f74ebe7045c1d09757c2dd047
Balamurugan <balamuruganelangovan.official@gmail.com>
2026-03-28 08:55:56 +0530

    added

---
+++ Random Quote Generator.py
@@ -0,0 +1,10 @@
+import random
+
+quotes = [
+    "Success is not final, failure is not fatal.",
+    "Believe in yourself.",
+    "Stay hungry, stay foolish.",
+    "Work hard in silence."
+]
+
+print("Quote:", random.choice(quotes))
